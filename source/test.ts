9b86d8570c8f6d5182d1fc431287a77a2a43709c
Pouya Kary <k@karyfoundation.org>
2017-10-04 16:49:07 +0330

    much and much more tests

--- source/test.ts
+++ source/test.ts
@@ -1,25 +1,54 @@
 
-/// <reference path="theory/scale.ts" />
+//
+// Copyright © 2017-present Pouya Kary <k@karyfoundation.org>
+//
+
 /// <reference path="theory/note.ts" />
+/// <reference path="theory/scale.ts" />
+/// <reference path="theory/chord.ts" />
 /// <reference path="lib/scales/major.ts" />
 /// <reference path="lib/scales/minor.ts" />
 
-const C =
-    new Tambourine.Note('C')
+//
+// ─── NOTES ──────────────────────────────────────────────────────────────────────
+//
 
-const CMajor =
-    new Tambourine.MajorScale( C )
+    const C = new Tambourine.Note( 'C' )
+    const E = new Tambourine.Note( 'E' )
 
-// Writing a chord with style
-const MajorTriad =
-    [ CMajor.Tonic, CMajor.Mediant, CMajor.Dominant ]
+    console.log('MIDI note number of 4th C is:', C.MIDI )
 
-const CMinor =
-    new Tambourine.MinorScale( C )
+//
+// ─── INTERVALS ──────────────────────────────────────────────────────────────────
+//
 
-const FavoriteBluesScaleForPouya =
-    new Tambourine.Scale( C, [ 3, 1, 1, 1, 1, 3 ] )
+    console.log( 'Interval between C and E is:', C.getIntervalTo( E ), 'half steps.' )
 
-console.log( FavoriteBluesScaleForPouya.getNotes( ) )
+//
+// ─── SCALES ─────────────────────────────────────────────────────────────────────
+//
 
+    const CMajor =
+        new Tambourine.MajorScale( C )
+
+    const CMinor =
+        new Tambourine.MinorScale( C )
+
+    const FavoriteBluesScaleForPouya =
+        new Tambourine.Scale( C, 3, 1, 1, 1, 1, 3 )
+
+    console.log('C Major has:', CMajor.noteNames )
+    console.log('C Minor has:', CMinor.noteNames )
+    console.log( "Pouya's favorite Blues Scale has:", FavoriteBluesScaleForPouya.noteNames )
+
+//
+// ─── CHORDS ─────────────────────────────────────────────────────────────────────
+//
+
+    const MajorTriad =
+        new Tambourine.Chord( CMajor.Tonic, CMajor.Mediant, CMajor.Dominant )
+
+    console.log( "Intervals of a major triad are:", MajorTriad.intervals )
+
+// ────────────────────────────────────────────────────────────────────────────────
 
