9c1f236d8028a32a6cebb6df04ed446bb9831fe7
Pouya Kary <k@karyfoundation.org>
2017-10-06 17:56:08 +0330

    pouya's awesome packaging

---
+++ source/index.ts
@@ -0,0 +1,22 @@
+
+//
+// Copyright © 2017-present Pouya Kary <k@karyfoundation.org>
+//
+
+/// <reference path="theory/note.ts" />
+/// <reference path="theory/scale.ts" />
+/// <reference path="theory/chord.ts" />
+/// <reference path="lib/scales/major.ts" />
+/// <reference path="lib/scales/minor.ts" />
+
+//
+// ─── EXPORT SYSTEM ──────────────────────────────────────────────────────────────
+//
+
+    if ( __dirname && module.exports && process.cwd ) {
+        module.exports = Tambourine
+    } else if ( window ) {
+        ( window as any ).Tambourine = Tambourine
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
