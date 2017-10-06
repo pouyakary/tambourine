f691a875fba0f82766da93cf10ced481495edf28
Pouya Kary <k@karyfoundation.org>
2017-10-06 17:48:56 +0330

    new tests added

--- source/test.ts
+++ source/test.ts
@@ -9,26 +9,32 @@
 /// <reference path="lib/scales/major.ts" />
 /// <reference path="lib/scales/minor.ts" />
 
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    const { Note, Chord, Scale, ScaleCollection} = Tambourine
+
 //
 // ─── TITLE ──────────────────────────────────────────────────────────────────────
 //
 
-    const put = ( ...args: any[ ] ) =>
-        console.log( '>', ...args )
+    const put = ( ...args: any[ ] ) => console.log( '>', ...args )
 
 //
 // ─── PARSING NOTE NAME ──────────────────────────────────────────────────────────
 //
 
-    put( 'Parsing G#6 note name:', Tambourine.Note.parseNoteName( 'G#6' ) )
+    put( 'Parsing G#6 note name:', Note.parseNoteName( 'G#6' ) )
 
 //
 // ─── NOTES ──────────────────────────────────────────────────────────────────────
 //
 
-    const C5 = new Tambourine.Note( 'C5' )
-    const E5 = new Tambourine.Note( 'E5' )
-    const A5 = new Tambourine.Note( 'A5' )
+    const C5 = new Note( 'C5' )
+    const E5 = new Note( 'E5' )
+    const A5 = new Note( 'A5' )
 
     put( 'MIDI note number of middle A (A5) is', A5.MIDI )
     put( 'Frequency of standard middle A (A5) is', A5.frequency )
@@ -37,33 +43,36 @@
 // ─── INTERVALS ──────────────────────────────────────────────────────────────────
 //
 
-    put( 'Interval between C and E is', C5.getIntervalTo( E5 ), 'half steps.' )
+    put( 'Interval between C and E is', C5.getDistanceTo( E5 ), 'half steps.' )
 
 //
 // ─── SCALES ─────────────────────────────────────────────────────────────────────
 //
 
-    const CMajor =
-        new Tambourine.MajorScale( C5 )
-
-    const CMinor =
-        new Tambourine.MinorScale( C5 )
-
-    const FavoriteBluesScaleForPouya =
-        new Tambourine.Scale( C5, 3, 1, 1, 1, 1, 3 )
+    const CMajor = new ScaleCollection.Major( C5 )
+    const CMinor = new ScaleCollection.Minor( C5 )
+    const FavoriteBluesScaleForPouya = new Scale( C5, 3, 1, 1, 1, 1, 3 )
 
     put( 'C Major has', CMajor.noteNames )
     put( 'C minor has', CMinor.noteNames )
     put( "Pouya's favorite Blues Scale has", FavoriteBluesScaleForPouya.noteNames )
 
 //
+// ─── SIGNATURES ─────────────────────────────────────────────────────────────────
+//
+
+    put( 'Key Signatures of E major Are',
+        new ScaleCollection.Major( new Note('E') ).keySignatures )
+
+//
 // ─── CHORDS ─────────────────────────────────────────────────────────────────────
 //
 
-    const MajorTriad =
-        new Tambourine.Chord( CMajor.Tonic, CMajor.Mediant, CMajor.Dominant )
+    const MajorTriad = new Chord( CMajor.Tonic, CMajor.Mediant, CMajor.Dominant )
 
     put( "Intervals of a major triad are", MajorTriad.intervals )
 
 // ────────────────────────────────────────────────────────────────────────────────
+
+
 
